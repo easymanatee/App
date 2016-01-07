2763ab132bf40ff42b9f8ced4f55738575e16bb6
easymanatee <patrikhartl@googlemail.com>
2016-01-07 21:17:26 +0100

    With all buttons

--- MyApp.py
+++ MyApp.py
@@ -2,13 +2,19 @@ import kivy
 kivy.require("1.9.1")
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.checkbox import CheckBox
 
-
-class MyApp(App):
+class Apps1(App):
+    icon = "vrreality-Logo1.0.png"
 
     def build(self):
-        return Label(text = "Hello World")
+        #self.title = "VR Reality News"
+        return FloatLayout()
+        
+    
+    
 
 
 if __name__ == "__main__":
-    MyApp().run()
+    Apps1().run()
